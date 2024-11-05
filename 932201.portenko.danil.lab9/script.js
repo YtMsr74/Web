ca277bacca477cbfd3066b3d17ca8e62c8137f16
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2024-11-05 20:43:18 +0700

    Add files via upload

---
+++ 932201.portenko.danil.lab9/script.js
@@ -0,0 +1,48 @@
+var start = true;
+var result = 0.0;
+var curNum = 0.0;
+var fSize = 38;
+display.textContent = '0';
+
+calculator.addEventListener('click', (event)=>{
+    if (event.target.tagName === 'BUTTON'){
+        if (display.textContent.length > 18) fSize -= 1.5;
+        else fSize = 38;
+        display.style.fontSize = fSize + 'px';
+    }
+})
+
+function clearRes(){
+    start = true;
+    display.textContent = '0';
+}
+
+function numClick(num){
+    if (start){
+        display.textContent = num;
+        start = false;
+    }
+    else display.textContent += num;
+}
+
+function opClick(op){
+    result = display.textContent;
+    display.textContent = result + " " + op + " ";
+}
+
+function dotClick(){
+    if (!display.textContent.includes('.')){
+        display.textContent += '.';
+    }
+}
+
+function backspace(){
+    text = display.textContent;
+    if (text.slice(-1) == ' ') display.textContent = text.slice(0,-2);
+    else display.textContent = text.slice(0,-1);
+    if (display.textContent == '') clearRes();
+}
+
+function calculate(){
+    display.textContent = eval(display.textContent);
+}
