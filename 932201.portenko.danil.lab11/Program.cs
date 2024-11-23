0ac9ecb52bdff6c75b613d4db1e5ac668631f17a
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2024-11-24 04:56:57 +0700

    Add files via upload

---
+++ 932201.portenko.danil.lab11/Program.cs
@@ -0,0 +1,26 @@
+using Calculator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICalcService, CalcService>();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
