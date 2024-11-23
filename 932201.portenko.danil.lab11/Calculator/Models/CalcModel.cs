d48a7dedefd5e3792addc37c99c69f2df44c19a4
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2024-11-24 05:10:36 +0700

    Add files via upload

---
+++ 932201.portenko.danil.lab11/Calculator/Models/CalcModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Models
+{
+    public class CalcModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Add { get; set; }
+        public int Sub { get; set; }
+        public int Mult { get; set; }
+        public string Div { get; set; }
+    }
+}
---
+++ 932201.portenko.danil.lab11/Calculator/Services/ICalcService.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Services
+{
+    public interface ICalcService
+    {
+        int Add (int num1, int num2);
+        int Sub (int num1, int num2);
+        int Mult (int num1, int num2);
+        string Div (int num1, int num2);
+    }
+}
